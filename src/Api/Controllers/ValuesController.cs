f2b571a8aed5d0765d5a9353346b658d0cfea399
Matt Jones <me@jonesm.io>
2019-03-11 21:03:38 +0100

    initial checkin

---
+++ src/Api/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
+    }
+}
